380fdcd29ff1e1dc1e9d456a530949c87d226a2e
alastairchet <alastair.chetcuti@gmail.com>
2014-03-17 13:00:05 +0100

    Re-committed

--- app/views.py
+++ app/views.py
@@ -242,6 +242,7 @@ def showallcamps():
     AllCamps = Campaign.query.all()
     return render_template('showallcamps.html', title='All Campaigns', Camps=AllCamps)
 
+
 @app.route('/savechanges')
 def setfunids():
     pgids = request.args.get('pgids')
@@ -251,6 +252,5 @@ def setfunids():
     funnel.content_ids = pgids
     db.session.commit()
     return redirect(url_for('managecamp', cid=c_id))
-
 
 
