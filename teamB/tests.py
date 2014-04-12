e288dd7eb9b3a6d15cbae66165a9b2e12e7fd5c3
nickgalea <nicholasgalea@gmail.com>
2014-04-12 13:05:58 +0200

    testing working test again

--- teamB/tests.py
+++ teamB/tests.py
@@ -10,13 +10,10 @@ class TestCase(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         app.config['CSRF_ENABLED'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
         self.app = app.test_client()
         db.create_all()
 
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
 
     def test_allowed_file(self):
         assert (views.allowed_file('abc.html')) == True
