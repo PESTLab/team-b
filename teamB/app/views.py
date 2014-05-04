1fff7456b5a1fbafd8dce121ada655d3e02fd1a9
nickgalea <nicholasgalea@gmail.com>
2014-05-04 14:29:56 +0200

    cookie name

--- teamB/app/views.py
+++ teamB/app/views.py
@@ -145,8 +145,7 @@ def broadcast(campname, productname, funnelname, pagetype):
     resp = make_response(render_template_string(rendered_page, p=mypage, pif = pinfunnel, tcode = testcode, f=funnel, title='Rendered Page'))
 
     if pinfunnel.test_pos != -1:
-        cookiename = 'variant' + str(mypage.test_id)
-        resp.set_cookie(cookiename, mypage.id)
+        resp.set_cookie('variant', mypage.id)
 
     return resp
 
